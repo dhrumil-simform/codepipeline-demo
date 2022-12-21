fef8ddb0916df1f94d616381880229adc31e3adb
dhrumil-simform <dhrumildalwadisimform@gmail.com>
2022-12-21 16:04:02 +0530

    fix test

--- src/components/Root/Counter/Counter.test.js
+++ src/components/Root/Counter/Counter.test.js
@@ -1,36 +1,26 @@
-import { fireEvent, render } from "@testing-library/react";
-import React from "react";
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
 
-import Counter from "./Counter";
+import Counter from './Counter';
 
-test("Counter component works properly", () => {
+test('Counter component works properly', () => {
   const { getByTestId, getByText } = render(<Counter />);
 
-  const counterValueEl = getByTestId("counter-value");
-  const minusTwoButtonEl = getByText("-2", { selector: "button" });
-  const minusOneButtonEl = getByText("-1", { selector: "button" });
-  const plusOneButtonEl = getByText("+1", { selector: "button" });
-  const plusTwoButtonEl = getByText("+2", { selector: "button" });
+  const counterValueEl = getByTestId('counter-value');
+  const minusOneButtonEl = getByText('-1', { selector: 'button' });
+  const plusOneButtonEl = getByText('+1', { selector: 'button' });
 
-  expect(counterValueEl).toHaveTextContent("0");
+  expect(counterValueEl).toHaveTextContent('0');
 
   fireEvent.click(minusOneButtonEl);
 
-  expect(counterValueEl).toHaveTextContent("-1");
-
-  fireEvent.click(minusTwoButtonEl);
-
-  expect(counterValueEl).toHaveTextContent("-3");
+  expect(counterValueEl).toHaveTextContent('-1');
 
   fireEvent.click(plusOneButtonEl);
 
-  expect(counterValueEl).toHaveTextContent("-2");
-
-  fireEvent.click(plusTwoButtonEl);
-
-  expect(counterValueEl).toHaveTextContent("0");
+  expect(counterValueEl).toHaveTextContent('0');
 
   fireEvent.click(plusOneButtonEl);
 
-  expect(counterValueEl).toHaveTextContent("1");
+  expect(counterValueEl).toHaveTextContent('1');
 });
